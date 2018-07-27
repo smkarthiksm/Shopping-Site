6cd8a060f6d1847c6035fe787edd8bd0152ff660
Karthik SM <karthik.sm@coda.global>
2018-07-27 11:50:39 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import * as Immutable from 'immutable';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+  ngAfterViewInit() {
+    $('.slider-nav').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      focusOnSelect:true,
+     // autoplay: true,
+      arrows:false,
+    });
+  }
 }
